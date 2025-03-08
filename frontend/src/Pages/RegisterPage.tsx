7f8b5d9f764d777fc005cb7ba7cac5fc49b5d10e
Muhammed Furkan GÜNEŞ <themfg.63@gmail.com>
2025-03-09 01:19:14 +0300

    Login page updated and register page created

---
+++ frontend/src/Pages/RegisterPage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const RegisterPage = () => {
+    return (
+        <div>Kayıt Sayfası</div>
+    )
+}
+
+export default RegisterPage
--- frontend/src/Routes/AppRoutes.tsx
+++ frontend/src/Routes/AppRoutes.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import {BrowserRouter,Route,Routes} from "react-router-dom"
 import AdminDashboard from '../Layout/AdminDashboard.tsx';
 import Random from '../Random.tsx';
+import LoginPage from '../Pages/LoginPage.tsx';
+import RegisterPage from '../Pages/RegisterPage.tsx';
 
 const AppRoutes = () => {
     return(
         <BrowserRouter>
             <Routes>
+                <Route path='/login' element={<LoginPage />} />
+                <Route path='/register' element={<RegisterPage />} />
                 <Route path='/' element={<AdminDashboard />}>
                     <Route path='/dashboard' element={<Random />} />
                     <Route path='/pharmacy' element={<Random />} />
